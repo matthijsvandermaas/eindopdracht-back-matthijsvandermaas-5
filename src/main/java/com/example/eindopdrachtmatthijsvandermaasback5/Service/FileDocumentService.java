d93fdc7c1c8cc66562963a8f3ac20e5db4276f17
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2024-01-30 21:03:35 +0100

    .

--- src/main/java/com/example/eindopdrachtmatthijsvandermaasback5/Service/FileDocumentService.java
+++ src/main/java/com/example/eindopdrachtmatthijsvandermaasback5/Service/FileDocumentService.java
@@ -5,6 +5,7 @@ import com.example.eindopdrachtmatthijsvandermaasback5.Models.Product;
 import com.example.eindopdrachtmatthijsvandermaasback5.Repository.FileDocumentRepository;
 import com.example.eindopdrachtmatthijsvandermaasback5.Repository.ProductRepository;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -13,10 +14,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 @Service
 public class FileDocumentService {
-
    private final FileDocumentRepository fileDocumentRepository;
 
    private ProductRepository productRepository;
@@ -26,31 +27,35 @@ public class FileDocumentService {
       this.productRepository = productRepository;
    }
 
-   public FileDocument uploadFileDocument(@RequestPart("file") MultipartFile file, String productName) throws IOException {
-      List<Product> productList = productRepository.findByProductName(productName);
-      if (productList.isEmpty()) {
-         throw new RuntimeException("Product not found for name: " + productName);
+   public FileDocument uploadFileDocument(@RequestPart("file") MultipartFile file, String productName) {
+      try {
+         List<Product> productList = productRepository.findByProductName(productName);
+         if (productList.isEmpty()) {
+            throw new RuntimeException("Product not found for name: " + productName);
+         }
+         Product product = productList.get(0);
+         product.setImages(null);
+         String originalFileName  = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+
+         FileDocument fileDocument = new FileDocument();
+         fileDocument.setFileName(originalFileName);
+         fileDocument.setDocFile(file.getBytes());
+
+         fileDocumentRepository.save(fileDocument);
+
+         product.setFileDocument(fileDocument);
+
+         productRepository.save(product);
+         System.out.println("Image uploaded successfully");
+         System.out.println(originalFileName);
+         System.out.println(fileDocument.getFileName());
+         return fileDocumentRepository.save(fileDocument);
+      } catch (IOException e) {
+         System.err.println("Image upload failed");
+         throw new RuntimeException("Image upload failed", e);
       }
-      Product product = productList.get(0);
-      product.setImages(null);
-      String originalFileName  = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
-
-      FileDocument fileDocument = new FileDocument();
-      fileDocument.setFileName(originalFileName );
-      fileDocument.setDocFile(file.getBytes());
-      //fileDocument.setProduct(product);
-
-      fileDocumentRepository.save(fileDocument);
-
-      product.setFileDocument(fileDocument);
-
-      productRepository.save(product);
-      System.out.println("Image uploaded successfully");
-      System.out.println(originalFileName);
-      System.out.println(fileDocument.getFileName());
-
-      return fileDocumentRepository.save(fileDocument);
    }
+
 
    public FileDocument singleFileDownload(String fileName, HttpServletRequest request){
 
